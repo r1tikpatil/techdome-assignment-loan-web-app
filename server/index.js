d045773790517e5c608cfa5f109f8836605a0c50
r1tikpatil <ritikpatil566@gmail.com>
2023-11-30 19:33:57 +0530

    created models

--- server/index.js
+++ server/index.js
@@ -2,10 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT;
 
 const db = require("./config/mongoose");
 
-const PORT = process.env.PORT;
+app.use(express.json());
+app.use("*", cors());
+
+
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
